e443aa3634396b51ed375681907fc4baa4cc7569
PupaeManU <istudents@iClassrooms-iMac-29.local>
2015-01-23 16:03:23 +0700

    CodeWeek2lab1

--- codeweek2-new.playground/section-1.swift
+++ codeweek2-new.playground/section-1.swift
@@ -22,15 +22,15 @@ class TipCalculator {
     
     
     func returnPossibleTips() -> [Int: Double] {
-    
+        
         let possibleTipsInferred = [0.15, 0.18, 0.20]
         let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
-       
+        
         var retval = Dictionary<Int, Double>()
         for possibleTip in possibleTipsInferred {
             let intPct = Int(possibleTip*100)
-        
-        retval[intPct] = calcTipWithTipPct(possibleTip)
+            
+            retval[intPct] = calcTipWithTipPct(possibleTip)
         }
         
         return retval
@@ -38,4 +38,3 @@ class TipCalculator {
 }
 let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
 tipCalc.returnPossibleTips()
-
