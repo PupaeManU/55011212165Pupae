fcd8717f8f0e8b0d2a4809d09f551c6008b93bb3
Nichaya062 <istudents@iClassrooms-iMac-20.local>
2015-03-06 14:10:57 +0700

    work

--- arrayfree.playground/section-1.swift
+++ arrayfree.playground/section-1.swift
@@ -3,25 +3,25 @@
 import UIKit
 
 class cafe{
-    var menu: [String] = ["Mocca", "Green tea", "Americano", "Capuchino", "Late"]
+    var menu : [String] = ["Mocca", "Greentea", "Americano", "Capuchino", "Late"]
     var price: [String] = ["40", "35", "30", "35", "40"]
-    
-    let menuprice :String
+    let pay :String
     
     init(menu2:String){
-        self.menuprice = menu2
+        self.pay = menu2
     }
     func cafeP() -> String {
         var out: String = ""
-        for(var i=0 ; i < menu.count ; i++){
-            if( menu[i] == menuprice )
-            
+        for(var i=0 ; i<=menu.count ; i++){
+            if(menu[i] == pay )
+                
             {
                 out = price[i];
             }
         }
-        return menuprice
+        return pay
     }
+    
 }
-let cafe2 = cafe(menu2:"Green tea")
+let cafe2 = cafe(menu2:"Greentea")
 cafe2.cafeP()
